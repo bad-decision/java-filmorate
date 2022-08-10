36b7c79a9cf33f73d4928631779317db0b730e60
Anton Zmeev <redsfanatic@yandex.ru>
2022-08-10 20:54:49 +0300

    fix: исправить замечания

--- src/test/java/ru/yandex/practicum/filmorate/FilmValidationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmValidationTest.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,15 +16,23 @@ import java.util.Set;
 public class FilmValidationTest {
 
     private final Validator validator;
+    private Film film;
 
     @Autowired
     public FilmValidationTest(Validator validator) {
         this.validator = validator;
     }
 
+    @BeforeEach
+    private void prepareCorrectFilm() {
+        film = new Film();
+        film.setDuration(100);
+        film.setName("Never give up");
+        film.setReleaseDate(LocalDate.of(1895, 12, 29));
+    }
+
     @Test
     public void validateIncorrectReleaseDate_mustReturnConstraint() {
-        Film film = getCorrectFilm();
         film.setReleaseDate(LocalDate.of(1895, 12, 28));
 
         Set<ConstraintViolation<Film>> constraintViolations = validator.validate(film);
@@ -33,16 +42,7 @@ public class FilmValidationTest {
 
     @Test
     public void validateCorrectReleaseDate_mustNotReturnConstraint() {
-        Film film = getCorrectFilm();
         Set<ConstraintViolation<Film>> constraintViolations = validator.validate(film);
         Assertions.assertEquals(0, constraintViolations.size());
-    }
-
-    private Film getCorrectFilm() {
-        Film film = new Film();
-        film.setDuration(100);
-        film.setName("Never give up");
-        film.setReleaseDate(LocalDate.of(1895, 12, 29));
-        return film;
     }
 }
